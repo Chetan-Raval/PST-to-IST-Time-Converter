062847b9690a38ad5d1e43c15e453b70c25b15a1
Chetan-Raval <chetan.iroidsolutions@gmail.com>
2024-08-20 17:24:10 +0530

    changes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path')
 const app = express();
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
