04663cfa48df7c080cc1aa4d0a9661c92ae64e64
Chetan-Raval <chetan.iroidsolutions@gmail.com>
2024-08-20 16:42:03 +0530

    changes

--- index.js
+++ index.js
@@ -46,22 +46,33 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.post('/', (req, res) => {
+    const { day, hour, minute, ampm } = req.body;
 
-app.get('/convert', (req, res) => {
-    const { day, hour, minute, ampm } = req.query;
-    // Combine hour, minute, and ampm to create time string
     const time = `${hour}:${minute} ${ampm}`;
-    
-    // Parse the day and time input
-    const dayOfWeek = moment().day(day); // This gets the next occurrence of the given day
+    const dayOfWeek = moment().day(day);
     const timePST = moment.tz(`${dayOfWeek.format('YYYY-MM-DD')} ${time}`, 'YYYY-MM-DD h:mm A', 'America/Los_Angeles');
-    
-    // Convert PST to IST
+
     const timeIST = timePST.clone().tz('Asia/Kolkata').format('dddd, h:mm A');
 
-    // Render the result page with the converted time
-    res.render('result', { timePST: timePST.format('dddd, h:mm A'), timeIST });
+    res.render('index', { timePST: timePST.format('dddd, h:mm A'), timeIST });
 });
+
+// app.get('/convert', (req, res) => {
+//     const { day, hour, minute, ampm } = req.query;
+//     // Combine hour, minute, and ampm to create time string
+//     const time = `${hour}:${minute} ${ampm}`;
+    
+//     // Parse the day and time input
+//     const dayOfWeek = moment().day(day); // This gets the next occurrence of the given day
+//     const timePST = moment.tz(`${dayOfWeek.format('YYYY-MM-DD')} ${time}`, 'YYYY-MM-DD h:mm A', 'America/Los_Angeles');
+    
+//     // Convert PST to IST
+//     const timeIST = timePST.clone().tz('Asia/Kolkata').format('dddd, h:mm A');
+
+//     // Render the result page with the converted time
+//     res.render('result', { timePST: timePST.format('dddd, h:mm A'), timeIST });
+// });
 
 
 // app.post('/convert', (req, res) => {
